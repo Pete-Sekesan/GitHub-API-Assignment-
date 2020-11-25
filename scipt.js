c6351ec709f7b8958bae997092f9d677e4f2d371
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-11-25 11:40:35 -0500

    js psuedocode/comments

---
+++ scipt.js
@@ -0,0 +1,20 @@
+
+
+//create a function to search for GitHub user name
+
+//link that function to the search-term ID field
+
+//clear out the search field after entering
+
+//find endpoint for searching GitHub user repos
+
+//display search results in <ul>
+
+//display repo name and have it be an clickable link to the repo
+
+//remove hidden class from section display-results ID
+
+//empty the display-results ID after new search
+
+//create an error message if no user name exists 
+
